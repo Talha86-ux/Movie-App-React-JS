f55844b198c015225c4e6ae06056afa21a2240d9
Talha86-ux <rjtalha555@gmail.com>
2026-02-16 03:43:21 +0500

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<div className="favorites-page">Favorites Page</div>} />
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<div className="favorites-page">Favorites Page</div>} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
